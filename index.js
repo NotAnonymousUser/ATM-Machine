e895f943ba296653ebe967fd9d0ab2a7fc4c4302
Muhammad Mubeen Javaid <125754246+NotAnonymousUser@users.noreply.github.com>
2024-03-28 15:36:07 +0500

    initial commits

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let myBalance = 100000;
+let myPin = 123;
+let quesPin = await inquirer.prompt({
+    name: "pin",
+    type: "number",
+    message: chalk.blue("enter you pin"),
+});
+if (quesPin.pin === myPin) {
+    console.log(chalk.green("your pin was correct"));
+    let atmOperations = await inquirer.prompt({
+        name: "operation",
+        type: "list",
+        choices: ["fast cash", "withdraw", "check balance"],
+    });
+    if (atmOperations.operation === "fast cash") {
+    }
+    if (atmOperations.operation === "withdraw") {
+    }
+    if (atmOperations.operation === "check balance") {
+    }
+}
+else {
+    console.log(chalk.red("Sorry your pin was not correct"));
+}
