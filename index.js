e95bddbc3e3693ff5fd8f876e0e514277ee6c3f4
Muhammad Mubeen Javaid <125754246+NotAnonymousUser@users.noreply.github.com>
2024-03-29 11:29:42 +0500

    updt

--- index.js
+++ index.js
@@ -20,12 +20,40 @@ if (quesPin.pin === myPin) {
     });
     if (atmOperations.operation === chalk.yellow("fast cash")) {
         console.log(`fast cash selected`);
+        let fastCash = await inquirer.prompt({
+            name: "fast",
+            type: "list",
+            message: "Choose an amount to withdraw",
+            choices: [1000, 5000, 10000, 20000, 50000],
+        });
+        if (fastCash.fast === 1000) {
+            myBalance -= 1000;
+        }
+        else if (fastCash.fast === 5000) {
+            myBalance -= 5000;
+        }
+        else if (fastCash.fast === 10000) {
+            myBalance -= 10000;
+        }
+        else if (fastCash.fast === 20000) {
+            myBalance -= 20000;
+        }
+        else if (fastCash.fast === 50000) {
+            myBalance -= 50000;
+        }
+        console.log(`Your Remaining Balance is ${myBalance}`);
     }
     else if (atmOperations.operation === chalk.blue("withdraw")) {
-        console.log(`withdraw selected`);
+        let withdrawCash = await inquirer.prompt({
+            name: "takeout",
+            type: "number",
+            message: "Enter your amount",
+        });
+        myBalance -= withdrawCash.takeout;
+        console.log(`Your Remaining Balance is ${myBalance}`);
     }
     else if (atmOperations.operation === chalk.green("check balance")) {
-        console.log(`Your Balance is ${myBalance}`);
+        console.log(`Your Remaining Balance is ${myBalance}`);
     }
 }
 else {
